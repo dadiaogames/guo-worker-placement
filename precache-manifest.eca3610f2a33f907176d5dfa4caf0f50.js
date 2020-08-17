21c6bd0aaede05cf6f0ce1191d3d19361cfb5cf0
jakehsiao <1499975101@qq.com>
2020-08-17 12:19:11 +0800

    Updates

---
+++ precache-manifest.eca3610f2a33f907176d5dfa4caf0f50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ec4be323553948b297e55641bfbb21",
+    "url": "/guo-worker-placement/index.html"
+  },
+  {
+    "revision": "d61b89fec84827553543",
+    "url": "/guo-worker-placement/static/css/main.9e9cc1c4.chunk.css"
+  },
+  {
+    "revision": "b21b48e11a50166f07df",
+    "url": "/guo-worker-placement/static/js/2.92990554.chunk.js"
+  },
+  {
+    "revision": "55aea68582af89c017bd4961013c0115",
+    "url": "/guo-worker-placement/static/js/2.92990554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61b89fec84827553543",
+    "url": "/guo-worker-placement/static/js/main.731543c3.chunk.js"
+  },
+  {
+    "revision": "7f55d38949c2c977bda9",
+    "url": "/guo-worker-placement/static/js/runtime-main.39e92136.js"
+  }
+]);
